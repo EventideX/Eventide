16b47e495942e4326525283277528085e5af2b98
EventideX <844058637@qq.com>
2017-01-14 02:00:33 +0800

    Add files via upload

---
+++ 未命名2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,n,a[10],b[10];
+    cin>>n;
+    for (i=0;i<n;i++)
+    {
+        cin>>a[i];
+        b[i]=a[i];
+    }
+    return 0;
+}
