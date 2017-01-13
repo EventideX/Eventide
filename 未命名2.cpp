2c47dc33933c9c3c30ab41e5981ccd46a1c8869b
EventideX <844058637@qq.com>
2017-01-14 02:06:02 +0800

    Add files via upload

---
+++ 未命名2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j,n,t,a[10];
+    cin>>n;
+    for (i=0;i<n;i++) cin>>a[i];
+    for (i=0;i<n-1;i++)
+    for (j=i+1;j<n;j++)
+    if (a[i]>a[j])      //从大到小为a[i]<a[j]
+    {
+        t=a[i];
+        a[i]=a[j];
+        a[j]=t;
+    }
+    return 0;
+}
